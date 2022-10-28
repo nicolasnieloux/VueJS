55708fceb4b4581ce22222d87078036d1dcff698
Nicolas Nieloux <nicolas.nieloux@Digital-Grenoble.local>
2022-10-28 10:57:00 +0200

    Utilisation de props

---
+++ src/components/City.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h3>{{ name }}</h3>
+    <p>{{ weather }}</p>
+    <p>{{ temperature }}</p>
+    <p>{{ updatedAt }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityTest",
+  props: {
+    msg: String,
+    name: String,
+    weather: String,
+    temperature: Number,
+    updatedAt: Date,
+  },
+  data() {
+    return {
+      // name: 'Ma ville', // nom de la ville
+      // weather: 'Peu nuageux', // descriptif météo
+      // temperature: 20.55, // température en °C
+      // updatedAt: new Date() // date de dernière mise à jour
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
